a2b5ed06f7d22a352149d23c8e043874c8c30bf5
“msamy250” <m.samyshaban@gmail.com>
2023-10-18 02:50:22 +0300

    new update

--- _printf.c
+++ _printf.c
@@ -1,50 +1,58 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
-int _printf(const char *format, ...){
+int _printf(const char *format, ...)
+{
+	int charct_num = 0;
+	va_list argmnt;
 
-    int charct_num = 0;
-    va_list argmnt;
+	if (format == NULL)
+		return (-1);
 
-    if (format == NULL)
-        return -1;
-    
-    va_start(argmnt, format);
+	va_start(argmnt, format);
 
-    while(*format)
-    {
-        if(*format != '%')
-        {
-            write(1, format, 1);
-            charct_num++;
-        }
-        else
-        {
-            format++;  
-        if(*format == '\0')
-            break;
-        if(*format == '%')
-        {
-          write(1, format, 1);
-            charct_num++;
-        }
-        else if(*format == 'c'){
-            char c = va_arg(argmnt, int);
-            write(1, &c, 1);
-            charct_num++;
-        }
-        else if(*format == 's'){
-            char *str = va_arg(argmnt, char*);
-            int len = 0;
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			charct_num++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				_putchar(*format);
+				charct_num++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(argmnt, int);
+				_putchar(c);
+				charct_num++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(argmnt, char *);
+				int len = 0;
 
-            while(str(len) != '\0')
-            lenth++;
-        write(1, str, len);
-        charct_num += len;
+				while (str[len] != '\0')
+					len++;
 
-        }
-        }
-        format++;
-    }
-    va_end(argmnt);
-    return charct_num;
+				write(1, str, len);
+
+				charct_num += len;
+			}
+		}
+
+		format++;
+	}
+
+	va_end(argmnt);
+	return (charct_num);
 }
