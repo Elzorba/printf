e07ca84bb7ad3b22b371b81982863cf3335fced7
moustafa yasser <mostafayassr21@gmail.com>
2023-10-17 14:20:34 -0700

    a

--- _printf.c
+++ _printf.c
@@ -1,49 +1,75 @@
 #include "main.h"
 
+/**
+ * print_op - function to check which specifier to print
+ * @format: string being passed
+ * @print_arr: array of struct ops
+ * @list: list of arguments to print
+ * Return: numb of char to be printed
+ */
+int print_op(const char *format, fmt_t *print_arr, va_list list)
+{
+	char a;
+	int count = 0, b = 0, c = 0;
+
+	a = format[b];
+	while (a != '\0')
+	{
+		if (a == '%')
+		{
+			c = 0;
+			b++;
+			a = format[b];
+			while (print_arr[c].type != NULL &&
+			       a != *(print_arr[c].type))
+				c++;
+			if (print_arr[c].type != NULL)
+				count = count + print_arr[c].f(list);
+			else
+			{
+				if (a == '\0')
+					return (-1);
+				if (a != '%')
+					count += _putchar('%');
+				count += _putchar(a);
+			}
+		}
+		else
+			count += _putchar(a);
+		b++;
+		a = format[b];
+	}
+	return (count);
+}
+
+/**
+ * _printf - prints output according to format
+ * @format: string being passed
+ * Return: char to be printed
+ */
 int _printf(const char *format, ...)
 {
-int charct_num = 0;
-va_list argmnt;
-if (format == NULL)
-        return (-1);
-    
-    va_start(argmnt, format);
+	va_list list;
+	int a = 0;
 
-    while(*format)
-    {
-        if(*format != '%')
-        {
-            write(1, format, 1);
-            charct_num++;
-        }
-        else
-        {
-            format++;  
-        if(*format == '\0')
-            break;
-        if(*format == '%')
-        {
-          write(1, format, 1);
-            charct_num++;
-        }
-        else if(*format == 'c'){
-            char c = va_arg(argmnt, int);
-            write(1, &c, 1);
-            charct_num++;
-        }
-        else if(*format == 's'){
-            char *str = va_arg(argmnt, char*);
-            int len = 0;
+	fmt_t ops[] = {
+		{"c", ch},
+		{"s", str},
+		{"d", _int},
+		{"b", _bin},
+		{"i", _int},
+		{"u", _ui},
+		{"o", _oct},
+		{"x", _hex_l},
+		{"X", _hex_u},
+		{"R", _rot13},
+		{NULL, NULL}
+	};
 
-            while(str(len) != '\0')
-            lenth++;
-        write(1, str, len);
-        charct_num += len;
-
-        }
-        }
-        format++;
-    }
-    va_end(argmnt);
-    return charct_num;
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	a = print_op(format, ops, list);
+	va_end(list);
+	return (a);
 }
--- main.h
+++ main.h
@@ -1,7 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct fmt - function to check for formats
+ * @type: The format to print
+ * @f: The print function to use
+ */
+typedef struct fmt
+{
+	char *type;
+	int (*f)();
+} fmt_t;
 
 int _printf(const char *format, ...);
-
+int print_op(const char *format, fmt_t *print_arr, va_list list);
+int ch(va_list character);
+int str(va_list string);
 #endif
